dcf8246f56f9b4bd659b0efe36ff942c7d8bf190
jenna <jenna_kim1@hotmail.com>
2019-06-04 17:22:39 +0200

    quest010

---
+++ front/src/SignUp.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'email',
+    };
+
+    this.updateEmailField = this.updateEmailField.bind(this);
+  }
+
+  updateEmailField(e) {
+    this.setState({email: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.email}</h1>
+        <input
+          type='email'
+          name='email'
+          placeholder='email'
+          onChange={this.updateEmailField}
+        />
+      </div>
+    );
+  }
+}

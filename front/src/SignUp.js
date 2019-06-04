9b2c92e94e0c6175531bab85d870cc14533b049f
jenna <jenna_kim1@hotmail.com>
2019-06-04 22:52:50 +0200

    quest020

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,26 +4,63 @@ export default class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'email',
+      email: '',
+      password: '',
+      passwordbis: '',
+      name: '',
+      lastname: '',
     };
 
     this.updateEmailField = this.updateEmailField.bind(this);
+    this.submitInfo = this.submitInfo.bind(this);
   }
 
   updateEmailField(e) {
-    this.setState({email: e.target.value});
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  submitInfo(e) {
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.email}</h1>
-        <input
-          type='email'
-          name='email'
-          placeholder='email'
-          onChange={this.updateEmailField}
-        />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.submitInfo}>
+          <input
+            type='email'
+            name='email'
+            placeholder='email'
+            onChange={this.updateEmailField}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            onChange={this.updateEmailField}
+          />
+          <input
+            type='password'
+            name='passwordbis'
+            placeholder='confirm password'
+            onChange={this.updateEmailField}
+          />
+          <input
+            type='text'
+            name='name'
+            placeholder='first name'
+            onChange={this.updateEmailField}
+          />
+          <input
+            type='text'
+            name='lastname'
+            placeholder='last name'
+            onChange={this.updateEmailField}
+          />
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }

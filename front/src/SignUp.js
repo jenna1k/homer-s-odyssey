459747666cc1782025971c8f23cd6b5214fe06db
jenna <jenna_kim1@hotmail.com>
2019-06-26 00:28:42 +0200

    completed quest060

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
@@ -14,18 +13,18 @@ export default class SignUp extends Component {
       passwordbis: '',
       name: '',
       lastname: '',
-      open: false,
+      // open: false,
     };
 
-    // this.updateEmailField = this.updateEmailField.bind(this);
+    this.updateEmailField = this.updateEmailField.bind(this);
     this.submitInfo = this.submitInfo.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleClose = this.handleClose.bind(this);
   }
 
-  // updateEmailField(e) {
-  //   this.setState({[e.target.name]: e.target.value});
-  // }
+  updateEmailField(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
   submitInfo(e) {
     e.preventDefault();
@@ -42,6 +41,7 @@ export default class SignUp extends Component {
         res => this.setState({flash: res.flash}),
         err => this.setState({flash: err.flash}),
       );
+    this.handleClick();
   }
 
   handleClick() {
@@ -57,8 +57,13 @@ export default class SignUp extends Component {
       <div>
         {/* <h1>{JSON.stringify(this.state, 1, 1)}</h1> */}
         <h2>Sign up!</h2>
-        <FormControl
-          style={{display: 'flex', flexWrap: 'wrap', margin: '10px'}}
+        <form
+          style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            margin: '10px',
+          }}
           onSubmit={this.submitInfo}>
           <TextField
             type='email'
@@ -90,30 +95,23 @@ export default class SignUp extends Component {
             label='last name'
             onChange={this.updateEmailField}
           />
-          {/* <div
-            style={{
-              margin: '5px',
-              display: 'flex',
-              justifyContent: 'flex-end',
-            }}> */}
-          {/* <input
+          <input
             type='submit'
             value='Submit'
-            id='submit-button'
+            id='submit-btn'
             style={{display: 'none'}}
           />
-          <label htmlFor='submit-button'> */}
-          <Button
-            type='submit'
-            value='Submit'
-            variant='contained'
-            color='primary'
-            component='span'
-            onClick={this.handleClick}
-            style={{margin: '5px', display: 'flex', alignSelf: 'flex-end'}}>
-            submit
-          </Button>
-          {/* </label> */}
+          <label htmlFor='submit-btn'>
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              component='span'
+              onClick={this.handleClick}
+              style={{margin: '5px', display: 'flex', alignSelf: 'flex-end'}}>
+              submit
+            </Button>
+          </label>
           <Snackbar
             anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
             open={this.state.open}
@@ -123,8 +121,7 @@ export default class SignUp extends Component {
             }}
             message={<span id='message-id'>{this.state.flash}</span>}
           />
-          {/* </div> */}
-        </FormControl>
+        </form>
       </div>
     );
   }
